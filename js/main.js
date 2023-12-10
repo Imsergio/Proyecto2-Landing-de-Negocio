3d031228a2f975990d83adf7ca9a18456a5e9b80
Imsergio <sergiodejesusuhmarquez@gmail.com>
2023-12-10 16:11:40 -0600

    Js para cerrar menu al hacer click en link

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
 const navMenu = document.querySelector('.nav'),
   navShow = document.querySelector('#nav-show'),
-  navClose = document.querySelector('#nav-close')
+  navClose = document.querySelector('#nav-close'),
+  navLink = document.querySelectorAll('.nav__link');
 /*=============== mostrar menu ===============*/
 
 navShow.addEventListener('click', () => {
@@ -11,4 +12,9 @@ navClose.addEventListener('click', () => {
   navMenu.classList.contains("show-menu") ? navMenu.classList.remove("show-menu") : navMenu.classList.add("show-menu")
 })
 
-/*=============== ** ===============*/
+/*=============== remover menu al hacer click en un link ===============*/
+
+const linkAction = ()=>{
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
