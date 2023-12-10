66b023c921c415659de81242fd650b71627066b6
Imsergio <sergiodejesusuhmarquez@gmail.com>
2023-12-10 16:32:51 -0600

    Animacion en el header al hacer scroll

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const navMenu = document.querySelector('.nav'),
   navClose = document.querySelector('#nav-close'),
   navLink = document.querySelectorAll('.nav__link');
 /*=============== mostrar menu ===============*/
-
+// cerrar o ocultar el menu al hacer click en los botones 
 navShow.addEventListener('click', () => {
   navMenu.classList.contains("show-menu") ? navMenu.classList.remove("show-menu") : navMenu.classList.add("show-menu")
 })
@@ -13,8 +13,17 @@ navClose.addEventListener('click', () => {
 })
 
 /*=============== remover menu al hacer click en un link ===============*/
-
+// al hacer click en un link se oculta el menu
 const linkAction = ()=>{
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*=============== Animacion al header al bajar en la pagina ===============*/
+// cuando el scroll es mayor a 50 se añade la clase.
+const scrollHeader = () =>{
+  const header = document.querySelector('.header');
+  this.scrollY >= 50 ? header.classList.add('bg-header')
+                      :header.classList.remove('bg-header');
+}
+window.addEventListener('scroll',scrollHeader);
